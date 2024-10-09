fa49cdf1304d49243c177af6c4533544e9aa6800
alexanderek <Alexander.ek@live.se>
2024-10-09 10:25:05 +0200

    Började med do-While metoder för säkerställa att man får försöka överföra eller ta ut pengar tills man angett korrekt input. Men valde enkel while loop istället.

--- Individuellt_Projekt_Bankomaten/obj/Debug/net8.0/Individuellt_Projekt_Bankomaten.AssemblyInfo.cs
+++ Individuellt_Projekt_Bankomaten/obj/Debug/net8.0/Individuellt_Projekt_Bankomaten.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Individuellt_Projekt_Bankomaten")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33f9e55ed65a2624c2b6c6927d159d2dcec8d801")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f563f4a4e828dc2ec13cdb16519170e0c33f416")]
 [assembly: System.Reflection.AssemblyProductAttribute("Individuellt_Projekt_Bankomaten")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Individuellt_Projekt_Bankomaten")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
